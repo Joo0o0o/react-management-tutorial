93ecbeee7ad9898c43f4591bf64074944d7420ac
Joo0o0o <jss0904@naver.com>
2021-12-18 09:21:10 +0900

    add get customer rest api

--- client/src/App.js
+++ client/src/App.js
@@ -22,36 +22,21 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    name: "홍길동",
-    image:
-      "https://img1.daumcdn.net/thumb/C500x500.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/6qYm/image/eAFjiZeA-fGh8Y327AH7oTQIsxQ.png",
-    gender: "male",
-    birthday: "19910904",
-    job: "thief",
-  },
-  {
-    id: 2,
-    name: "풍월량",
-    image: "https://cdn.gametoc.co.kr/news/photo/201710/45338_85463_759.JPG",
-    gender: "male",
-    birthday: "19840124",
-    job: "bj",
-  },
-  {
-    id: 3,
-    name: "침착맨",
-    image:
-      "https://w.namu.la/s/2cd5110fc9e9e2f8c67f05fa5fb1d7f20957a29c80203d0c63864960640fd8b7d618ee394c9efb04d0da86512ea9a7b6a9adb7422ea2c82051286c31c975c3351ba05ed8dfabe0b3be5520292e8e09bd",
-    gender: "male",
-    birthday: "19870902",
-    job: "streamer",
-  },
-];
-
 class App extends Component {
+  state = { customers: "" };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -69,18 +54,20 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              {customers.map((c) => {
-                return (
-                  <Customer
-                    id={c.id}
-                    name={c.name}
-                    image={c.image}
-                    gender={c.gender}
-                    birthday={c.birthday}
-                    job={c.job}
-                  />
-                );
-              })}
+              {this.state.customers
+                ? this.state.customers.map((c) => {
+                    return (
+                      <Customer
+                        id={c.id}
+                        name={c.name}
+                        image={c.image}
+                        gender={c.gender}
+                        birthday={c.birthday}
+                        job={c.job}
+                      />
+                    );
+                  })
+                : ""}
             </TableBody>
           </Table>
         </Paper>
